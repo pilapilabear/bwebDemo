59560c04f216603769fa8bc1c0d5ac71aa0b3a8d
bilabilabear <bilabilabear@12.com>
2020-05-01 22:16:52 +0800

    -

--- src/main/java/com/example/demo/error/CommonError.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.error;
-
-public interface CommonError {
-    public int GetErrorCode();
-    public String GetErrorMessage();
-    public CommonError SetErrorMessage(String errorMessage);
-}
--- src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.service.model.UserModel;
-public interface UserService {
-    UserModel GetUserById(Integer id);
-}
